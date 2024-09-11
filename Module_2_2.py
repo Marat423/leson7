6b087c8f3fff5d112eec4064d075eb1b42ad821b
Marat <msaltysh@gmail.com>
2024-09-11 17:41:33 +0500

    Add files via upload

---
+++ Module_2_2.py
@@ -0,0 +1,10 @@
+first = int(input()) # Проссим ввести число
+second = int(input()) # Проссим ввести число
+third = int(input()) # Проссим ввести число
+
+if first == second and first == third: # Если все числа равны то 3
+    print(3)
+elif first == second or first == third: # Если два числа равно то 2
+    print(2)
+else: # Иначе 0
+    print(0)
