3195b00b3653489fcc71b71f30129fe04e70dc61
Marat <msaltysh@gmail.com>
2024-09-11 18:17:34 +0500

    Update Module_2_2.py

--- Module_2_2.py
+++ Module_2_2.py
@@ -2,9 +2,9 @@ first = int(input()) # Проссим ввести число
 second = int(input()) # Проссим ввести число
 third = int(input()) # Проссим ввести число
 
-if first == second or first == third or second == third: # Если два числа равно то 2
-    print(2)
-elif first == second and first == third: # Если все числа равны то 3
+if first == second and first == third: # Если все числа равны то 3
     print(3)
+elif first == second or first == third or second == third: # Если два числа равно то 2
+    print(2)   
 else: # Иначе 0
     print(0)
